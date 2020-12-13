9f75fde5c9f4c14356c814d474cdff3582e706a3
JaimePintoP <jaimepinto.saxo@gmail.com>
2020-12-13 12:09:50 +0100

    Add createPost page and functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar/Navbar';
-import LandingPage from './pages/LandingPage/LandingPage';
-import Signup from './pages/Signup/Signup';
-import Login from './pages/Login/Login';
-import Profile from './pages/Profile/Profile';
+import Navbar from "./components/Navbar/Navbar";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import Profile from "./pages/Profile/Profile";
 
-import AnonRoute from './components/AnonRoute';
-import PrivateRoute from './components/PrivateRoute';
-import EditProfile from './pages/EditProfile/EditProfile';
-import Dashboard from './pages/Dashboard/Dashboard';
-import PostDetails from './pages/PostDetails/PostDetails';
-import MyPosts from './pages/MyPosts/MyPosts';
-
-
-
-
+import AnonRoute from "./components/AnonRoute";
+import PrivateRoute from "./components/PrivateRoute";
+import EditProfile from "./pages/EditProfile/EditProfile";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import PostDetails from "./pages/PostDetails/PostDetails";
+import MyPosts from "./pages/MyPosts/MyPosts";
+import CreatePost from "./pages/CreatePost/CreatePost";
 
 class App extends Component {
   render() {
@@ -28,17 +25,24 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={LandingPage} />
 
-
           <AnonRoute exact path="/signup" component={Signup} />
           <AnonRoute exact path="/login" component={Login} />
 
           {/* <PrivateRoute exact path="/private" component={Private} /> */}
           <PrivateRoute exact path="/profile/:userId" component={Profile} />
-          <PrivateRoute exact path="/editProfile/:userId" component={EditProfile} />
+          <PrivateRoute
+            exact
+            path="/editProfile/:userId"
+            component={EditProfile}
+          />
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
-          <PrivateRoute exact path="/postDetails/:postId" component={PostDetails} />
+          <PrivateRoute
+            exact
+            path="/postDetails/:postId"
+            component={PostDetails}
+          />
           <PrivateRoute exact path="/myPosts/:userId" component={MyPosts} />
-
+          <PrivateRoute exact path="/createPost" component={CreatePost} />
         </Switch>
       </div>
     );
---
+++ src/pages/CreatePost/CreatePost.js
@@ -0,0 +1,133 @@
+import axios from "axios";
+import React from "react";
+import { withAuth } from "../../context/auth-context";
+import { Link } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
+
+class CreatePost extends React.Component {
+  state = {
+    title: undefined,
+    country: undefined,
+    city: undefined,
+    description: undefined,
+    image: undefined,
+    isReady: false,
+  };
+
+  //   componentDidMount() {
+  //     // const { postId } = this.props.match.params;
+  //   }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    const { title, country, city, description, image } = this.state;
+    const { userId } = this.props.match.params;
+    console.log("userId", userId);
+
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URI}/api/createPost`,
+        { title, country, city, description, image },
+        { withCredentials: true }
+      )
+      .then(() => {
+        this.props.history.push(`/dashboard`);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleFileUpload = (e) => {
+    console.log("The file to be uploaded is: ", e.target.files);
+    const file = e.target.files[0];
+
+    const uploadData = new FormData();
+    // image => this name has to be the same as in the model since we pass
+    // req.body to .create() method when creating a new project in '/api/projects' POST route
+    uploadData.append("image", file);
+
+    axios
+      .post(`${process.env.REACT_APP_API_URI}/api/upload`, uploadData, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log("response is: ", response);
+        // after the console.log we can see that response carries 'secure_url' which we can use to update the state
+        // this.setState({ image: response.data.secure_url });
+        this.setState({ image: response.data.secure_url, isReady: true });
+      })
+      .catch((err) => {
+        console.log("Error while uploading the file: ", err);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Create a Post</h2>
+        </div>
+        <div>
+          <form onSubmit={this.handleFormSubmit}>
+            <label>Title:</label>
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+
+            <label>Country:</label>
+            <input
+              type="text"
+              name="country"
+              value={this.state.country}
+              onChange={this.handleChange}
+            />
+
+            <label>City:</label>
+            <input
+              type="text"
+              name="city"
+              value={this.state.city}
+              onChange={this.handleChange}
+            />
+
+            <label>Description:</label>
+            <textarea
+              name="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+
+            <label>Image</label>
+            <input
+              name="image"
+              type="file"
+              onChange={this.handleFileUpload}
+            ></input>
+            <span>
+              <img
+                style={{ width: "100px" }}
+                src={this.state.image && this.state.image}
+                alt=""
+              ></img>
+            </span>
+
+            <input
+              type="submit"
+              value="Submit"
+              disabled={!this.state.isReady}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withAuth(CreatePost);

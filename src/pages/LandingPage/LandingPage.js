d002000ab50f0ca1c5d705ed2794c4f9f67de2d7
JaimePintoP <jaimepinto.saxo@gmail.com>
2020-12-14 13:37:35 +0100

    Add links and text on LandingPage.

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
@@ -6,9 +7,26 @@ function LandingPage() {
       <h1>Home Page</h1>
 
       <div>header image</div>
+      <div>
+        <Link to={"/signup"}>
+          <button>Sign Up</button>
+        </Link>
 
+        <Link to={"login"}>
+          <button>Login</button>
+        </Link>
+      </div>
+
+      <h4>What is Travel Guru?</h4>
+      <p>
+        Travel Guru is a platform where you can interact with other users about
+        travel experiences. Create a travel post to share your wonderful trips,
+        comment on other users posts and add them to your favorites!. Also you
+        have a private section dedicated for your next travel ideas, go and
+        check your Travel Log!
+      </p>
     </div>
-  )
+  );
 }
 
 export default LandingPage;

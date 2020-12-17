acfdf3f4376398cce1b75a300bea2f2bb3acb9a9
Lucy Quinn <lucy.quinn.uk@gmail.com>
2020-12-17 10:00:07 +0100

    fix footer - parent relative, footer relative.

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="full-page">
         <Navbar />
         <Switch>
           <AnonRoute exact path="/" component={LandingPage} />

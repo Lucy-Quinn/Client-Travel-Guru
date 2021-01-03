922225724d2e6ec3137408369eec2bf1132fc094
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-03 13:13:52 +0100

    in createCommment component make form field description empty after handleformsubmit.

--- src/components/CreateComment/CreateComment.js
+++ src/components/CreateComment/CreateComment.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { withAuth } from "../../context/auth-context";
-
 import './CreateComment.css';
 
 class CreateComment extends React.Component {
@@ -10,17 +9,14 @@ class CreateComment extends React.Component {
   };
 
   handleChange = (event) => {
-
-    // event.preventDefault();
-
     const { name, value } = event.target;
     this.setState({ [name]: value });
   };
 
   handleFormSubmit = (event) => {
     event.preventDefault();
-
     this.props.handleFormSubmit(this.state.description)
+    this.setState({ description: '' })
   }
 
   render() {
@@ -28,14 +24,11 @@ class CreateComment extends React.Component {
       <div>
         <form className="comment-form"
           onSubmit={this.handleFormSubmit}>
-          {/* onSubmit={() => this.props.handleFormSubmit(this.state.description)}> */}
-
           <textarea
             name="description"
             value={this.state.description}
             onChange={this.handleChange}
           />
-
           <button className="comment-button" type="submit">
             Send
           </button>

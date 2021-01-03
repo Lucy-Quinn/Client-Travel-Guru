f11eb4d618fd8f2c228e7e5ed604811b1cc4c631
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-03 17:09:38 +0100

    css styling for 768px, 1024px and 1440px for postDetails and comment components.

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -7,7 +7,12 @@ class Comment extends React.Component {
     render() {
         return (
             <div className="comment-container">
-                <p>{this.props.postDetails.commentAuthor.username}</p>
+
+                <div className="comment-author-container">
+                    <img src={this.props.postDetails.commentAuthor.image} alt="comment author" />
+
+                    <p className="comment-author-name">{this.props.postDetails.commentAuthor.username}</p>
+                </div>
                 <p className="comment-text">{this.props.postDetails.description}</p>
             </div>
         )
